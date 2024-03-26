f61a07a8792c51368c7c21cf00408cfae800ec6b
Devon Cash <devon.p.cash@gmail.com>
2024-03-26 14:44:17 -0700

    added weapons

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,28 +1,90 @@
-import { z } from 'astro/zod';
-import { defineCollection, reference } from 'astro:content';
+import { z } from "astro/zod";
+import { defineCollection, reference } from "astro:content";
+
+const TableItem = z.object({
+  name: z.string(),
+  desc: z.string().optional(),
+  extra: z.array(z.string()).optional(),
+});
+
+const Table = z.object({
+  distribution: z.enum(["normal", "uniform"]).optional().default("uniform"),
+  items: z.union([z.array(z.string()), z.array(TableItem)]),
+});
+
+const ArmorTable = Table.extend({
+  item_type: z.literal("armor"),
+  items: z.array(
+    TableItem.extend({
+      armor: z.number().optional(),
+      equipAsWeapon: z.boolean().optional(),
+      accessory: z.boolean().optional(),
+    }),
+  ),
+});
+
+const WeaponTable = Table.extend({
+  item_type: z.literal("weapon"),
+  items: z.array(
+    TableItem.extend({
+      damage: z.string().optional(),
+    }),
+  ),
+});
+
+const VehicleTable = Table.extend({
+  item_type: z.literal("vehicle"),
+  items: z.array(
+    TableItem.extend({
+      hp: z.number(),
+      armor: z.number(),
+    }),
+  ),
+});
 
 const tables = defineCollection({
-    type: 'data',
-    schema: z.union([z.array(z.any()), z.object({
-        distribution: z.enum(['normal', 'uniform']),
-        items: z.array(z.any()),
-    })]).transform((val) => {
-        if(Array.isArray(val)) return { distribution: 'uniform', items: val }
-        return val;
-    })
+  type: "data",
+  schema: z.union([
+    z
+      .discriminatedUnion("item_type", [ArmorTable, WeaponTable, VehicleTable])
+      .transform((table) => {
+        if (table.item_type) {
+          for (const item of table.items) {
+            item.item_type = table.item_type;
+          }
+        }
+        return table;
+      }),
+    Table,
+    z.array(z.string()).transform((items) => ({
+      distribution: "uniform",
+      items,
+    })),
+  ]),
 });
 
 const generators = defineCollection({
-    type: 'data',
-    schema: z.object({
-        singular: z.string(),
-        plural: z.string(),
-        tables: z.record(reference('tables'))
-    })
-})
+  type: "data",
+  schema: z.object({
+    singular: z.string(),
+    plural: z.string(),
+    tables: z.record(
+      z.union([
+        z.object({
+          table: reference("tables"),
+          count: z.number().optional().default(1),
+        }),
+
+        reference("tables").transform((ref) => ({
+          table: ref,
+          count: 1,
+        })),
+      ]),
+    ),
+  }),
+});
 
 export const collections = {
-    tables,
-    generators
-}
-
+  tables,
+  generators,
+};
--- src/pages/generate/[...path]/index.json.ts
+++ src/pages/generate/[...path]/index.json.ts
@@ -3,35 +3,39 @@ import { getCollection, getEntry } from "astro:content";
 import triangular from "@stdlib/random-base-triangular";
 
 function rand_uniform<T>(array: Array<T>): T {
+  if (!Array.isArray(array)) throw new Error("Expected an array, got " + array);
   return array[Math.floor(Math.random() * array.length)];
 }
 
 function rand_normal<T>(array: Array<T>): T {
+  if (!Array.isArray(array)) throw new Error("Expected an array, got " + array);
   return array[Math.floor(triangular(0, array.length, array.length / 2))];
 }
 
 async function RollTableCategory(
   category: string,
   entry?: string,
-  n: number = 1
+  n: number = 1,
 ) {
-  const tables = await getCollection(
-    "generators",
-    entry
-      ? ({ id }) => id.startsWith(`${category}/${entry}`)
-      : ({ id }) => id.startsWith(category)
+  const tables = await getCollection("generators", ({ id }) =>
+    id.startsWith(entry ? `${category}/${entry}` : category),
   );
 
   const results: any[] = [];
   for (let i = 0; i < n; i++) {
     const generator = rand_uniform(tables);
-
     const result: any = {};
     for (const trait in generator.data.tables) {
-      const { data: table } = await getEntry(generator.data.tables[trait]);
+      const { data: table } = await getEntry(
+        generator.data.tables[trait].table,
+      );
       const selector =
         table.distribution === "normal" ? rand_normal : rand_uniform;
-      result[trait] = selector(table.items) ?? [];
+      const picked = [];
+      for (let j = 0; j < generator.data.tables[trait].count; j++) {
+        picked.push(selector(table.items));
+      }
+      result[trait] = picked;
     }
     results.push({
       table: generator.id,
